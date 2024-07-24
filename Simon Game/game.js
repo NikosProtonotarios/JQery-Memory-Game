326e3885a536644f6b1594a2db46f2924f62646b
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-07-24 12:25:17 +0100

    add soundPlay function

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,21 +1,26 @@
-let buttonColours = ["green", "red", "yellow", "blue"];
-let gamePattern = [];
-let userClickedPattern = [];
+$(document).ready(function () {
+  let buttonColours = ["green", "red", "yellow", "blue"];
+  let gamePattern = [];
+  let userClickedPattern = [];
 
-function nextSequence() {
+  function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $('#' + randomChosenColour).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour)
+      .fadeOut(100)
+      .fadeIn(100);
     playSound(randomChosenColour);
-}
-nextSequence();
+  }
+  nextSequence();
 
-function playSound(name) {
+  function playSound(name) {
     let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
-}
+  }
 
-function handler() {
-    let userChosenColour = 
-}
+  $("div.btn").click(function () {
+    let curentButton = $("div").fadeOut(100).fadeIn(100);
+    console.log(curentButton);
+  });
+});
