478e760b81f1a565d480a97846db9a3ee3468042
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-07-24 11:28:10 +0100

    add a playsound function

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,9 +1,21 @@
 let buttonColours = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $('#' + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 }
 nextSequence();
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function handler() {
+    let userChosenColour = 
+}
